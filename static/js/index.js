067b8d49b57773efb52543e72f3fe0188ef369b4
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-03-18 15:33:09 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,48 @@
 //https://www.eclipse.org/paho/clients/js/
 
+ 
+
 function LED1_On() {
-	alert("led on");
-	console.log("led on");
-	document.getElementById("sensor").innerHTML="led on";
+    alert("led on");
+    console.log("led on");
+    document.getElementById("sensor").innerHTML="led on";
+    message = new Paho.MQTT.Message("ON");
+    message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+    client.send(message);
   
 }
-function LED1_Off(){	
-	alert("led off");
-	console.log("led off");
-	document.getElementById("sensor").innerHTML="led off";
+function LED1_Off(){    
+    alert("led off");
+    console.log("led off");
+    //document.getElementById("sensor").innerHTML="led off";
+    message = new Paho.MQTT.Message("OFF");
+    message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+    client.send(message);
 }
 
+ 
 
-
-
-
+ 
 
 Create a client instance
-client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
+ client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+
+ 
 
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "bryan.loaiza@unach.edu.ec",
+    password: "123",
     onSuccess:onConnect,
     onFailure:doFail
   }
+
+ 
 
   // connect the client
   client.connect(options);
@@ -40,28 +51,34 @@ client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    
+    client.subscribe("bryan.loaiza@unach.edu.ec/tema1");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
     client.send(message);
-	
-  }
-//
- // function doFail(e){
-  //  console.log(e);
-	
+    
   }
 
+ 
+
+  function doFail(e){
+    console.log(e);
+    
+  }
+
+ 
+
   // called when the client loses its connection
- // function onConnectionLost(responseObject) {
-  //  if (responseObject.errorCode !== 0) {
- //     console.log("onConnectionLost:"+responseObject.errorMessage);
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
 
+ 
+
   // called when a message arrives
- // function onMessageArrived(message) {
-  //  console.log("onMessageArrived:"+message.payloadString);
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+      document.getElementById("sensor").innerHTML=message.payloadString;
   }
-  //
