100894a776c4e2b9c168122d1eb4f8fc3a7146c4
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-04-12 09:57:29 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,86 +1,76 @@
-//https://www.eclipse.org/paho/clients/js/
 
- 
-
-function senso1() {
-    alert("led on");
-    //console.log("led on");
-    //document.getElementById("sensor").innerHTML="led on";
-    message = new Paho.MQTT.Message("ON");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
-    client.send(message);
-  
-}
-
-function senso2(){    
-    alert("led off");
-    //console.log("led off");
-    //document.getElementById("sensor").innerHTML="led off";
-    message = new Paho.MQTT.Message("OFF");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
-    client.send(message);
-  
-}
-
- 
-
- 
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
- 
-
-  // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "kevinguadalupe15@gmail.com",
-    password: "kevin2000",
+    userName: "bryan.loaiza@unach.edu.ec",
+    password: "123",
     onSuccess:onConnect,
     onFailure:doFail
   }
 
- 
-
-  // connect the client
+  
   client.connect(options);
    
-  // called when the client connects
+ 
   function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-    
-    client.subscribe("kevinguadalupe15@gmail.com/tema1");
-    message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
-    client.send(message);
-    
+    client.subscribe("bryan.loaiza@unach.edu.ec/tema1");
+    client.subscribe("bryan.loaiza@unach.edu.ec/tema3");
+	
   }
 
- 
 
   function doFail(e){
     console.log(e);
-    
+	
   }
 
- 
 
-  // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
 
- 
+
+
+function senso1() {
+	alert("led on");
+	//console.log("led on");
+	message = new Paho.MQTT.Message("sensor1");
+	message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+	client.send(message);
+  
+}
+
+
+
+function senso2(){	
+	alert("led off");
+	//console.log("led off");
+	message = new Paho.MQTT.Message("sensor2");
+	message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+	client.send(message);
+
+}
+
+
 
   // called when a message arrives
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-      document.getElementById("sensor").innerHTML=message.payloadString;
+	  if (message.destinationName=="bryan.loaiza@unach.edu.ec/tema1"){
+		  document.getElementById("sensor1").innerHTML=message.payloadString;
+	  
+	  }
+	  
+	  if (message.destinationName=="bryan.loaiza@unach.edu.ec/tema3"){
+		  document.getElementById("sensor2").innerHTML=message.payloadString;
+	  
+	  }
+
+
   }
+
