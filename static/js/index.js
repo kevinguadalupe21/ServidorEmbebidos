438a6c5f6de06c124b238ed96a2e197d34bc22b5
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-04-12 00:26:05 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@ function senso1() {
     //console.log("led on");
     //document.getElementById("sensor").innerHTML="led on";
     message = new Paho.MQTT.Message("ON");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
+    message.destinationName = "kevinguadalupe15@gmail.com/tema1";
     client.send(message);
   
 }
@@ -16,7 +16,7 @@ function senso2(){
    // console.log("led off");
     //document.getElementById("sensor").innerHTML="led off";
     message = new Paho.MQTT.Message("OFF");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
+    message.destinationName = "kevinguadalupe15@gmail.com/tema1";
     client.send(message);
   
 }
@@ -55,7 +55,7 @@ function senso2(){
     
     client.subscribe("kevinguadalupe15@gmail.com/tema2");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
+    message.destinationName = "kevinguadalupe15@gmail.com/tema1";
     client.send(message);
     
   }
