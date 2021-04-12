323d5f65f3442814a530c1dfd9e593b15a9b20e4
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-04-12 10:00:27 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "bryan.loaiza@unach.edu.ec",
-    password: "123",
+    userName: "kevinguadalupe15@gmail.com",
+    password: "kevin2000",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -17,8 +17,8 @@
  
   function onConnect() {
     console.log("Conectado...");
-    client.subscribe("bryan.loaiza@unach.edu.ec/tema1");
-    client.subscribe("bryan.loaiza@unach.edu.ec/tema3");
+    client.subscribe("kevinguadalupe15@gmail.com/tema1");
+    client.subscribe("kevinguadalupe15@gmail.com/tema3");
 	
   }
 
@@ -41,7 +41,7 @@ function senso1() {
 	alert("led on");
 	//console.log("led on");
 	message = new Paho.MQTT.Message("sensor1");
-	message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+	message.destinationName = "kevinguadalupe15@gmail.com/tema2";
 	client.send(message);
   
 }
@@ -52,7 +52,7 @@ function senso2(){
 	alert("led off");
 	//console.log("led off");
 	message = new Paho.MQTT.Message("sensor2");
-	message.destinationName = "bryan.loaiza@unach.edu.ec/tema2";
+	message.destinationName = "kevinguadalupe15@gmail.com/tema2";
 	client.send(message);
 
 }
@@ -61,12 +61,12 @@ function senso2(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-	  if (message.destinationName=="bryan.loaiza@unach.edu.ec/tema1"){
+	  if (message.destinationName=="kevinguadalupe15@gmail.com/tema1"){
 		  document.getElementById("sensor1").innerHTML=message.payloadString;
 	  
 	  }
 	  
-	  if (message.destinationName=="bryan.loaiza@unach.edu.ec/tema3"){
+	  if (message.destinationName=="kevinguadalupe15@gmail.com/tema3"){
 		  document.getElementById("sensor2").innerHTML=message.payloadString;
 	  
 	  }
