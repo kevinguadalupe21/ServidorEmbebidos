b12e3c19637702f78d37fbb1aa19edcecc261c7e
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-04-11 19:37:47 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
+function senso1() {
 	alert("led on");
 	//console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
@@ -8,9 +8,9 @@ function LED1_On() {
    	message.destinationName = "kevinguadalupe15@gmail.com/tema2";
     	client.send(message);
 }
-function LED1_Off(){	
+function senso2(){	
 	alert("led off");
-	console.log("led off");
+	//console.log("led off");
 	//document.getElementById("sensor").innerHTML="led off";
 	message = new Paho.MQTT.Message("OFF");
     	message.destinationName = "kevinguadalupe15@gmail.com/tema2";
