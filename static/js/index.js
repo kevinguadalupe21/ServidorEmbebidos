e06c80e005d72880937d8159d01add69fe9bbaed
kevinguadalupe21 <80278232+kevinguadalupe21@users.noreply.github.com>
2021-04-11 23:47:48 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,50 @@
 //https://www.eclipse.org/paho/clients/js/
 
+ 
+
 function LED1_On() {
-	alert("led on");
-	console.log("led on");
-	document.getElementById("sensor").innerHTML="led on";
+    /alert("led on");/
+    console.log("led on");
+    //document.getElementById("sensor").innerHTML="led on";
+    message = new Paho.MQTT.Message("ON");
+    message.destinationName = "kevinguadalupe15@gmail.com/tema1";
+    client.send(message);
   
 }
-function LED1_Off(){	
-	alert("led off");
-	console.log("led off");
-	document.getElementById("sensor").innerHTML="led off";
-}
+/*
+function LED1_Off(){    
+    alert("led off");
+    console.log("led off");
+    //document.getElementById("sensor").innerHTML="led off";
+    message = new Paho.MQTT.Message("OFF");
+    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
+    client.send(message);
+  
+}*/
 
+ 
 
-
-
-
+ 
 
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
+ 
+
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "kevinguadalupe15@gmail.com",
+    password: "kevin2000",
     onSuccess:onConnect,
     onFailure:doFail
   }
+
+ 
 
   // connect the client
   client.connect(options);
@@ -40,18 +53,22 @@ function LED1_Off(){
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    
+    client.subscribe("kevinguadalupe15@gmail.com/tema1");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "kevinguadalupe15@gmail.com/tema2";
     client.send(message);
-	
+    
   }
+
+ 
 
   function doFail(e){
     console.log(e);
-	
+    
   }
+
+ 
 
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
@@ -60,8 +77,10 @@ function LED1_Off(){
     }
   }
 
+ 
+
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
+      document.getElementById("sensor").innerHTML=message.payloadString;
   }
-  
